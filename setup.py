5dc415b3e726188170d8dd0d9cc42aead7f367f4
brunodemgomes <brunomgomes@icloud.com>
2022-12-15 15:52:28 -0300

    CLI and setup.py updated

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name='acppred',
     version='0.0.1',
     packages=find_packages(),
-    author='Bruno Gomes'
+    author='Bruno Gomes',
+    entry_points = {
+        'console_scripts': [
+            'acppred_train = acppred.train:main',
+            'acppred_predict = acppred.predict:main'
+        ]
+    }
 )
